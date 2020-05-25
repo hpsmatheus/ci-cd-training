f46fb53ad7346db86dc40f941e6c9649ea78a9f8
Matheus Henrique Pereira Santos <lamatheus.santos@MacBook-Pro-de-Matheus.local>
2020-05-25 11:56:30 -0300

    Pipeline now has one test passing and one failing

---
+++ src/test/java/com/example/demo/FailTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FailTest {
+
+    @Test
+    void mustFail() {
+        assert (1==2);
+    }
+}
