9c11ebfe61a2d9f234bdab93d45d2f082b04fb2c
Matheus Henrique Pereira Santos <lamatheus.santos@MacBook-Pro-de-Matheus.local>
2020-05-25 11:51:38 -0300

    Pipeline must be green after some refactoring

--- src/test/java/com/example/demo/SuccessTest.java
+++ src/test/java/com/example/demo/SuccessTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SuccessTest {
 
 	@Test
-	void contextLoads() {
-		//Teste de pipeline
+	void mustSucceed() {
 		assert (1==1);
 	}
 
